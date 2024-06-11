dc97436e372b1c711e117397e033df939fe7221e
Ivan Dauhalaptseu <ivan.dauhalaptseu@gmail.com>
2024-06-11 23:49:19 +0300

    feat: Spring Boot Rest API returns List

--- src/main/java/com/progtammingtechie/productionpractice/controller/StudentController.java
+++ src/main/java/com/progtammingtechie/productionpractice/controller/StudentController.java
@@ -4,6 +4,8 @@ import com.progtammingtechie.productionpractice.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class StudentController {
     @GetMapping("/student")
@@ -15,5 +17,17 @@ public class StudentController {
         strudent.setLastName("Dolgolaptev");
 
         return strudent;
+    }
+
+
+    @GetMapping("/listStudents")
+    public List<Student> getListStudents() {
+        List<Student> students = List.of(
+                new Student(1, "Ivan", "Dolgolaptev"),
+                new Student(2, "Petr", "Petrov"),
+                new Student(3, "Sidor", "Sidorov")
+        );
+
+        return students;
     }
 }
