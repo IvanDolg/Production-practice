e269f9020275081b7b13ce50e1e5f74f4a7b4994
Ivan Dauhalaptseu <ivan.dauhalaptseu@gmail.com>
2024-06-11 12:11:08 +0300

    init: start working

---
+++ src/test/java/com/progtammingtechie/productionpractice/ProductionPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progtammingtechie.productionpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
