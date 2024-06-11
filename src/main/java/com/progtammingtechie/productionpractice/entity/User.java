8acf2444e46a0e3ef506bc53d7067877bf4257ff
Ivan Dauhalaptseu <ivan.dauhalaptseu@gmail.com>
2024-06-12 01:25:54 +0300

    feat: create User JPA Entity

---
+++ src/main/java/com/progtammingtechie/productionpractice/entity/User.java
@@ -0,0 +1,28 @@
+package com.progtammingtechie.productionpractice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "second_name")
+    private String secondName;
+
+    @Column(name = "email", unique = true)
+    private String email;
+}
