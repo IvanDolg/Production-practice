aeff45237c775de4202a7d370716b9920d478430
Ivan Dolgolaptev <ivandolgolaptev@gmail.com>
2024-06-12 12:19:00 +0300

    init: test commit

--- src/main/java/com/progtammingtechie/productionpractice/ProductionPracticeApplication.java
+++ src/main/java/com/progtammingtechie/productionpractice/ProductionPracticeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductionPracticeApplication {
+public class  ProductionPracticeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductionPracticeApplication.class, args);
