95b72430e3bb4e6816de34166ec984421e1b8d0c
Ivan Dolgolaptev <ivandolgolaptev@gmail.com>
2024-06-12 16:50:58 +0300

    init: add postgre sql in docker

--- src/test/java/com/progtammingtechie/productionpractice/ProductionPracticeApplicationTests.java
+++ src/test/java/com/progtammingtechie/productionpractice/ProductionPracticeApplicationTests.java
@@ -3,7 +3,7 @@ package com.progtammingtechie.productionpractice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ProductionPracticeApplicationTests {
 
     @Test
