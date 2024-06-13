82188fc28526d8dec638e536bd4fb61f61b35db2
Ivan Dolgolaptev <ivandolgolaptev@gmail.com>
2024-06-13 12:32:06 +0300

    feat: Spring boot REST API global exception handling

--- src/main/java/com/progtammingtechie/productionpractice/exception/GlobalExtensionHandler.java
+++ src/main/java/com/progtammingtechie/productionpractice/exception/GlobalExtensionHandler.java
@@ -20,6 +20,7 @@ public class GlobalExtensionHandler {
                 "USER_NOT_FOUND");
         return new  ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
+
     @ExceptionHandler(EmailAlreadyExistsException.class)
     public ResponseEntity<ErrorDetails> handleEmailAlreadyExistsException(EmailAlreadyExistsException exception,
                                                                           WebRequest webRequest) {
@@ -29,5 +30,16 @@ public class GlobalExtensionHandler {
                 webRequest.getDescription(false),
                 "USER_EMAIL_ALREADY_EXISTS");
         return new  ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception,
+                                                              WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDateTime.now(),
+                exception.getMessage(),
+                webRequest.getDescription(false),
+                "INTERNAL SERVER ERROR");
+        return new  ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
